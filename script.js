28b4ae7b6522ceedd255fb21b7d3e88cc903fe84
tarunmahata21 <mahatatarun12@gmail.com>
2024-08-27 22:21:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function shownav() {
+    var show = document.querySelector('.hide')
+    show.style.display='flex'
+}
+function hidenav(){
+    var hide= document.querySelector('.hide')
+    hide.style.display='none'
+}
